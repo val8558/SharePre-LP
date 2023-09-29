115787b2e4768417c90572e84353abb48d646094
Valmir <111082887+val8558@users.noreply.github.com>
2023-09-29 19:38:58 -0300

    Change Commits

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -10,7 +10,17 @@ function Body() {
                 <h1>Comece a construir seu portfólio de <br />criptomoedas</h1>
                 <span>Comece de forma rápida e segura a construir seu portfólio com a SharePre.</span>
                     <a href="http://api.whatsapp.com/send?phone=5511957877222" target='_blank'>Começar</a>
-                
+            </div>
+
+            <div className={style.cryptoContainer}>
+                <div>
+                    <h2>Criptomoedas populares</h2>
+                <div className={style.cryptoTrend}>
+                    <div>
+
+                    </div>
+                </div>
+                </div>
             </div>
         </>
     )
